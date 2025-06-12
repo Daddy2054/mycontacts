4d570498234bec83f7c35c721f0be2d81a532115
Daddy2054 <stivali-tinte-0w@icloud.com>
2025-06-12 20:42:15 +0200

    Initial Commit

---
+++ mycontacts/mycontactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mycontactsApp.swift
+//  mycontacts
+//
+//  Created by Jean on 12/06/25.
+//
+
+import SwiftUI
+
+@main
+struct mycontactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
